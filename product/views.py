2c16f36d784201933c4c788117ef97e67b8278b1
approved <tuan0990@gmail.com>
2020-10-25 19:49:54 +0700

    Product listing

--- product/views.py
+++ product/views.py
@@ -8,8 +8,16 @@ from .models import ProductListingPage, ProductDetailPage
 
 def product_listing(request):
     all_product_categories = ProductListingPage.objects.live().public().exact_type(ProductListingPage)
-    context = {'categories':[]}
+    all = [];
+    category = {};
+    category["all"] = [];
+    category["bean"] = [];
+    category["whey"] = [];
+    context = {'categories':category}
     for product_category in all_product_categories:
-        context["categories"].append({"title": product_category.title, "products": product_category.get_children().live().public().specific})
+        all.append({"title": product_category.title, "products": product_category.get_children().live().public().specific})
 
+    category["all"] = all;
+    category["bean"] = all;
+    category["whey"] = all;
     return render(request, 'product/product_listing_page.html', context)
